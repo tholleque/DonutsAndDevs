367a053b9f5a6c3f48d8ba40d158ccec31bf2a67
Tim Holleque <timothy.t.holleque@gmail.com>
2023-05-31 21:08:56 -0400

    services

--- src/app/hofapi.service.ts
+++ src/app/hofapi.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Complete } from './HallOfFame';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +10,10 @@ export class HOFAPIService {
 
   url:string= "https://grandcircusco.github.io/demo-apis/computer-science-hall-of-fame.json";
 
-  constructor(http:HttpClient) { }
+  constructor(private http:HttpClient) { }
 
+ getDevs():Observable<Complete>{
+  return this.http.get<Complete>(this.url);
+ }
  
 }
